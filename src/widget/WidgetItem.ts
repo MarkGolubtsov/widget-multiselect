0cb580e698cac6c6d356392dd261c6d20180769a
Mark <glmarkgl4@gmail.com>
2021-02-27 03:09:26 +0300

    Created WidgetItem.

---
+++ src/widget/WidgetItem.ts
@@ -0,0 +1,26 @@
+export class WidgetItem {
+    private readonly _value: string
+    private _isSelected: boolean
+
+    constructor(item: string, isSelected = false) {
+        this._value = item;
+        this._isSelected = isSelected;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    select() {
+        this._isSelected = true;
+    }
+
+    unselect() {
+        this._isSelected = false;
+    }
+
+    get isSelected() {
+        return this._isSelected
+    }
+}
+
