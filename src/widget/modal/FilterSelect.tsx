a9661d270adbeee9c07e69120e61df3c316e6db5
Mark <glmarkgl4@gmail.com>
2021-02-27 03:11:46 +0300

    Created Select for filter.

---
+++ src/widget/modal/FilterSelect.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Select} from 'antd';
+import {useStore} from 'widget/modal/SelectModal';
+import {Filter, filtersAction} from 'widget/store/filter';
+import {changeFilter} from 'widget/store/actions';
+
+const {Option} = Select;
+export const FilterSelect = () => {
+    const [state, dispatch] = useStore();
+
+    const handleChange = (value: Filter) => {
+        dispatch(changeFilter(value));
+    }
+
+    const filters = Array.from(Object.keys(filtersAction));
+
+    return (
+        <Select value={state.filter} onChange={handleChange}>
+            {filters.map(value => {
+                return <Option value={value}>{value}</Option>
+            })}
+        </Select>
+    )
+}
